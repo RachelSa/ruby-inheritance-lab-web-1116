d1382c9b47dfcf5a4ef8b3431728f381fb01cce2
github username <github email address>
2016-12-05 11:26:32 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(learning)
+    @knowledge << learning
+  end
+
+  def knowledge
+    @knowledge 
+  end
+
+  def teach
+    len = @knowledge.length 
+    random = rand(0..len)
+    @knowledge[random]
+  end
 
 end
